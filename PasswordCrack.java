d0097e22cd7cdaefd1a8c2ee3ad348d6904bc943
Kyle Martinez <kyle.martinez@blackbaud.com>
2014-12-02 11:15:33 -0600

    parse out information from password file, now we need to crack the passwords

--- PasswordCrack.java
+++ PasswordCrack.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class PasswordCrack {
@@ -13,10 +15,35 @@ public class PasswordCrack {
 		try{
 			dictonary = new BufferedReader(new FileReader(args[0]));
 			passwords = new BufferedReader(new FileReader(args[1]));
+            crackPasswords();
 			dictonary.close();
 			passwords.close();
 		}catch (Exception e){
 			e.printStackTrace();
 		}
 	}
+
+    public static void crackPasswords() throws IOException{
+        Pattern pattern = Pattern.compile("[a-z]*:(.*):\\d*:\\d*:([a-zA-Z]*)\\s([a-zA-Z]*):.*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher;
+        String line;
+        String salt;
+        String encryptedPassword;
+        String firstName;
+        String lastName;
+        while ((line = passwords.readLine()) != null){
+            //System.out.println(line);
+            matcher = pattern.matcher(line);
+            if (matcher.matches()){
+                encryptedPassword = matcher.group(1);
+                firstName = matcher.group(2);
+                lastName = matcher.group(3);
+                salt = encryptedPassword.substring(0, 2);
+//                System.out.println(firstName);
+//                System.out.println(lastName);
+//                System.out.println(encryptedPassword);
+//                System.out.println(salt);
+            }
+        }
+    }
 }
